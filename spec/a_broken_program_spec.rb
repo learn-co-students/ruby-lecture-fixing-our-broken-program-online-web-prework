7a4529c4372dade2977c7a56139df66c47692d7a
legacy-concept-8860 <miala0925@yahoo.com>
2019-03-12 02:42:45 +0000

    Done.

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -1,9 +1,8 @@
-describe 'A broken program' do
+puts "A broken program" do
 
-  it 'raises an error when loaded' do
-    expect{
-      load 'lib/a_broken_program.rb'
+puts "raises an error when loaded" do
+   expect{
+  puts "lib/a_broken_program.rb"
     }.to raise_error
-  end
-
+    
 end
